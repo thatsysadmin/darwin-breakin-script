45b94e25e76efc181d5cbc5c3ede7b99f9a51916
thatsysadmin <h@XThreeSixty.elasticsky.lan>
2023-10-04 12:13:39 -0700

    another try.

--- darwin-breakin-script.sh
+++ darwin-breakin-script.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+echo "Breakin' in."
 DSCL_DB="/Volumes/Macintosh HD/var/db/dslocal/nodes/Default"
 USERNAME="breakin"
 PASSWORD=$1
-UID="101"
-GID="20"
+USERID="101"
+GROUPID="20"
 
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UserShell /bin/zsh
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME RealName $USERNAME
-dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UniqueID "$UID"
-dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME PrimaryGroupID "$GID"
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UniqueID "$USERID"
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME PrimaryGroupID "$GROUPID"
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME NFSHomeDirectory /Users/$USERNAME
 dscl -f "$DSCL_DB" localonly -passwd /Local/Target/Users/$USERNAME $PASSWORD
 dscl -f "$DSCL_DB" localonly -append /Local/Target/Groups/admin GroupMembership $USERNAME
