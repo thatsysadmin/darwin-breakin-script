dfe7b4d0634becd1bbd3602cb113117117377036
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2024-02-07 20:37:33 -0800

    Another try.

--- darwin-breakin-script.sh
+++ darwin-breakin-script.sh
@@ -9,9 +9,9 @@ GROUPID="20"
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UserShell /bin/zsh
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME RealName $USERNAME
-dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UniqueID "$USERID"
-dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME PrimaryGroupID "$GROUPID"
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UniqueID $USERID
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME PrimaryGroupID $GROUPID
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME NFSHomeDirectory /Users/$USERNAME
-dscl -f "$DSCL_DB" localonly -passwd /Local/Target/Users/$USERNAME $PASSWORD
+dscl -f "$DSCL_DB" localonly -passwd /Local/Target/Users/$USERNAME "$PASSWORD"
 dscl -f "$DSCL_DB" localonly -append /Local/Target/Groups/admin GroupMembership $USERNAME
 echo "Done."
